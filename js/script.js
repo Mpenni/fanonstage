6455e5d75637ed6628bde889d1934de108dfe367
mpenny <mmcflymail@gmail.com>
2023-09-07 17:23:04 +0200

    Add Javascript code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Define Elements:
+const wideImg = document.querySelector('.full-width-image-atf img'),
+  captionText = wideImg.getAttribute('alt'),
+  captionInnerHTML = '<span class="image-caption">' + captionText + '</span>',
+  caption = document.createElement('div');
+// Edit:
+  caption.setAttribute('class', 'image-caption-wrap');
+  caption.style.cssText = 'background: none;';
+  caption.innerHTML = captionInnerHTML;
+// Insert on page:
+  wideImg.parentNode.appendChild(caption);
