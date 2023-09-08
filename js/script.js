b19e8da2b65cf450a9fc2f8e81930aba8e2222f6
mpenny <mmcflymail@gmail.com>
2023-09-08 09:32:44 +0200

    add js dynamic content insert

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+window.onload = function() {
+
+  
+  //var video001 = ;
+  var name_001 = "Foo Fighters";
+  var description_001 = "A brave boy with Kiss makeup picks up a guitar and juggles dexterously on stage!";
+  //var date_001 =;
+  var city_001 = "Austin";
+  var country_001 = "USA";
+  var fan_001 = "Yayo Sanchez";
+
+  document.getElementById('name_001').innerHTML = name_001;
+  document.getElementById('description_001').innerHTML = description_001;
+  document.getElementById('city_001').innerHTML = city_001;
+  document.getElementById('country_001').innerHTML = country_001;
+  document.getElementById('fan_001').innerHTML = fan_001;
+
+
+
+
+};
+
+
+// Img caption
 // Define Elements:
 const wideImg = document.querySelector('.full-width-image-atf img'),
   captionText = wideImg.getAttribute('alt'),
